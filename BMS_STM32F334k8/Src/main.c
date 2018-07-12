fa7c193eda1e6dbd09aeb478697b83b728a06b72
DonatodAcunto <docdone00@gmail.com>
2018-07-12 18:39:35 +0200

    up

--- BMS_STM32F334k8/Src/main.c
+++ BMS_STM32F334k8/Src/main.c
@@ -6,7 +6,7 @@
   ******************************************************************************
   ** This notice applies to any and all portions of this file
   * that are not between comment pairs USER CODE BEGIN and
-  * USER CODE END. Other portions of this file, whether 
+  * USER CODE END. Other portions of this file, whether
   * inserted by the user or by software development tools
   * are owned by their respective copyright owners.
   *
@@ -51,6 +51,23 @@
 #include <stdlib.h>
 #include <inttypes.h>
 
+#define CAN_ID_PACK 0xAA
+
+#define CAN_ID_STOP 0x04
+#define CAN_ID_TOTVOL 0x01
+#define CAN_ID_ERROR 0x08
+#define CAN_ID_TS_ON 0x03
+#define CAN_ID_INIT 0x02
+#define CAN_ID_CURRENT 0x05
+#define CAN_ID_TEMP 0x06
+#define CAN_ID_MAX_TEMP 0x07
+
+#define TOT_IC 12 // number of daisy chain
+#define CELL_CH 9
+#define NUM_RX_BYT 8u
+#define BYT_IN_REG 6u
+#define CELL_IN_REG 3u
+#define NUM_CV_REG 3u
 
 CAN_FilterConfTypeDef sFilter;
 CanRxMsgTypeDef RxHeader;
@@ -70,6 +87,37 @@ SPI_HandleTypeDef hspi1;
 /* Private variables ---------------------------------------------------------*/
 
 
+
+ uint8_t cell_data[32];
+ uint16_t cell_voltages[108];
+ uint16_t cell_temps[108];
+ uint16_t parsed_cell;
+ uint16_t received_pec;
+ uint16_t data_pec;
+ uint8_t pec_error=0;
+ uint16_t voltages[9];
+ int counterCicle = 0;
+ float parsed1;
+ float parsed2;
+ float parsed3;
+ float parsed4;
+ float parsed5;
+ float parsed6;
+ float parsed7;
+ float parsed8;
+ float parsed9;
+ float *av_temp1 = 0;
+ float *av_temp2 = 0;
+ float *av_temp3 = 0;
+ uint32_t *adcBuffer;
+ float gigi[9];
+ uint8_t *totalPack;
+ uint32_t Current;
+ uint8_t *tractiveVoltage;
+ uint16_t *max_vol;
+ uint16_t *min_vol;
+ float *average_vol;
+ uint16_t temp[9];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -82,6 +130,14 @@ static void MX_I2C1_Init(void);
 
 /* USER CODE BEGIN PFP */
 /* Private function prototypes -----------------------------------------------*/
+int GetMSB(int intValue)
+{
+   return (intValue & 0xFFFF0000);
+}
+int GetLSB(int intValue)
+{
+  return (intValue & 0x0000FFFF);
+}
 
 /* USER CODE END PFP */
 
@@ -160,26 +216,10 @@ int main(void)
 
 
 
-  	int id = 0;
 
  	 // GPIO Logic
  	 //Precharge off
-  	HAL_Delay(500);
-  	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_SET); 	//BMS fault high
-//	HAL_Delay(10000);
-//	HAL_Delay(10000);
-  	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_SET);		//PRECHARGE
-  	HAL_Delay(1);
-  	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
-  	HAL_Delay(10000);
-  	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_SET);	//TS_ON
-  	HAL_Delay(10000);
-  	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_RESET);
-  	HAL_Delay(10000);
-  	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_RESET);	//BMS_FAULT
-  	HAL_Delay(10);
-  	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_SET);
-  	HAL_Delay(1000000000000);
+ 	// HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
 // 	 GPIO_PinState ShutDown_Status;
 // 	 ShutDown_Status = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_6);
 // 	 if(ShutDown_Status == GPIO_PIN_SET){
@@ -211,8 +251,130 @@ int main(void)
 // 	uint32_t Current = Get_Amps_Value(adcBuffer,(uint16_t)offset);
 
 
+  	 /* ----- Voltages ------*/
+  		  	 ltc6804_adcv(MD_7KHZ_3KHZ, DCP_DISABLED, CELL_CH_ALL, hspi1);
+  		  	 HAL_Delay(10);
+  			 uint8_t data_counter = 0;
+  			 for(uint8_t current_ic = 0; current_ic < TOT_IC; current_ic++){
 
-  			 HAL_Delay(500);
+  					 ltc6804_rdcv_reg(current_ic, TOT_IC, cell_data, hspi1);
+
+  			 	 	 array_voltages(voltages, cell_data);
+
+
+
+//  			 	 	char num[2];
+//  			 	 	sprintf(num, "\n");
+  			 	 //	HAL_UART_Transmit(&huart2, &num, strlen(num), 100);
+  			 	 	 for(int i = 0; i < 9; i++){
+//  			 	 		 char v[32];
+//  			 	 		 sprintf(v, "%d - ",voltages[i]);
+//  			 	 		 HAL_UART_Transmit(&huart2, &v, strlen(v), 100);
+  			 	 		 cell_voltages[current_ic*9+i] = voltages[i];
+  			 	  	 }
+
+
+
+  			 }
+  			 //char num[8];
+
+  			 max_min_voltages(cell_voltages, max_vol, min_vol, average_vol);
+  	//		 //Can Messages
+  	//
+  	//		 /* ----- Temperatures -----*/
+  	//
+  	//		 //odd temp
+
+  			 ltc6804_address_temp_odd(MD_7KHZ_3KHZ, DCP_DISABLED, CELL_CH_ALL, hspi1);
+  			 HAL_Delay(1);
+  			 ltc6804_adcv_temp(MD_7KHZ_3KHZ, DCP_DISABLED, CELL_CH_ALL, hspi1);
+  			 HAL_Delay(10);
+  			 for(uint8_t current_ic = 0 ; current_ic < TOT_IC; current_ic++){
+  				 ltc6804_rdcv_temp(current_ic, TOT_IC, cell_data, hspi1);
+  				 	 array_temp_odd(temp, cell_data);
+
+  				 	 cell_temps[current_ic*9+0] = (uint16_t)(convert_temp(temp[0])*100);
+  				 	 cell_temps[current_ic*9+2] = (uint16_t)(convert_temp(temp[2])*100);
+  				 	 cell_temps[current_ic*9+4] = (uint16_t)(convert_temp(temp[4])*100);
+  				 	 cell_temps[current_ic*9+6] = (uint16_t)(convert_temp(temp[6])*100);
+  				 	 cell_temps[current_ic*9+8] = (uint16_t)(convert_temp(temp[8])*100);
+
+  			 }
+
+
+  			 //
+  	//		 //ltc6804_rdcv_temp(...);
+  	//		 convert_temp();
+  	//
+  	//		 //even temp
+  			 ltc6804_address_temp_even(MD_7KHZ_3KHZ, DCP_DISABLED, CELL_CH_ALL, hspi1);
+  			 HAL_Delay(1);
+  			 ltc6804_adcv_temp(MD_7KHZ_3KHZ, DCP_DISABLED, CELL_CH_ALL, hspi1);
+  			 HAL_Delay(10);
+  			 for(uint8_t current_ic = 0; current_ic < TOT_IC; current_ic++){
+  				 ltc6804_rdcv_temp(current_ic, TOT_IC, cell_data, hspi1);
+  			 		 array_temp_even(temp, cell_data);
+
+  			 		 cell_temps[current_ic*9+1] = (uint16_t)(convert_temp(temp[1])*100);
+  			 		 cell_temps[current_ic*9+3] = (uint16_t)(convert_temp(temp[3])*100);
+  			 		 cell_temps[current_ic*9+5] = (uint16_t)(convert_temp(temp[5])*100);
+  			 		 cell_temps[current_ic*9+7] = (uint16_t)(convert_temp(temp[7])*100);
+
+  			 }
+  			ltc6804_stop_temp(MD_7KHZ_3KHZ, DCP_DISABLED, CELL_CH_ALL, hspi1);
+//  			 for(int i = 0; i < 9; i++){
+//  					 char v[32];
+//  					 sprintf(v, "%x - ",cell_temps[7][i]);
+//  					 HAL_UART_Transmit(&huart2, &v, strlen(v), 100);
+//  					 }
+
+
+  	//		 // Controllo Temperatura massima
+  	//		 uint16_t *max_temp = 0;
+  	//		 if(counterCicle % 3 == 0){
+  	//			 max_ave_temp(cell_codes_temp, max_temp, av_temp1);
+  	//		 }
+  	//		 if(counterCicle % 3 == 1){
+  	//		 	 max_ave_temp(cell_codes_temp, max_temp, av_temp2);
+  	//		 }
+  	//		 if(counterCicle % 3 == 2){
+  	//		 	 max_ave_temp(cell_codes_temp, max_temp, av_temp3);
+  	//		 }
+  	//		 float average = 0;
+  	//		 if(counterCicle > 2){
+  	//
+  	//		 average = (*av_temp1 + *av_temp2 + *av_temp3)/3;
+  	//
+  	//
+  	//
+  	//		 if(average < 60 && *max_vol*0.0001f < 4.20 && *min_vol*0.0001f > 2.80){
+  	//			 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_SET);
+  	//		 }
+  	//		 }
+  	//		 float **converted_temp;
+  	//		 for(int i = 0; i < 12; i++){
+  	//			 for(int j = 0; j < 9; j++){
+  	//				 converted_temp[i][j] = convert_temp(cell_codes_temp[i][j]);
+  	//			 }
+  	//		 }
+  	//
+  	//CAN MESSAGES
+//  			 uint16_t tot_vol = total_pack_voltage(cell_voltages);
+
+  	//		 CAN_Send(id,(uint8_t)max_vol, 8, hcan);
+  	////		 CAN_Send(id,(uint8_t)min_vol, 8, hcan);
+
+//  			 CAN_Send(CAN_ID_PACK,CAN_ID_TOTVOL,tot_vol , 8, hcan);
+
+  			// CAN_Send(CAN_ID_PACK, CAN_ID_ERROR,error, 8, hcan);
+//  			 CAN_Send(CAN_ID_PACK, CAN_ID_CURRENT,(uint8_t)Current, 8, hcan);
+
+  			//CAN_Send(CAN_ID_PACK,CAN_ID_INIT,tot_vol , 8, hcan);
+  			//CAN_Send(CAN_ID_PACK,CAN_ID_TS_ON,tot_vol , 8, hcan);
+  			//CAN_Send(CAN_ID_PACK,CAN_ID_STOP,tot_vol , 8, hcan);
+
+  		//	 CAN_Send(id,(uint8_t)average, 8, hcan);
+
   	  }
   /* USER CODE END 3 */
 
@@ -229,7 +391,7 @@ void SystemClock_Config(void)
   RCC_ClkInitTypeDef RCC_ClkInitStruct;
   RCC_PeriphCLKInitTypeDef PeriphClkInit;
 
-    /**Initializes the CPU, AHB and APB busses clocks 
+    /**Initializes the CPU, AHB and APB busses clocks
     */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
@@ -242,7 +404,7 @@ void SystemClock_Config(void)
     _Error_Handler(__FILE__, __LINE__);
   }
 
-    /**Initializes the CPU, AHB and APB busses clocks 
+    /**Initializes the CPU, AHB and APB busses clocks
     */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
@@ -264,11 +426,11 @@ void SystemClock_Config(void)
     _Error_Handler(__FILE__, __LINE__);
   }
 
-    /**Configure the Systick interrupt time 
+    /**Configure the Systick interrupt time
     */
   HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
 
-    /**Configure the Systick 
+    /**Configure the Systick
     */
   HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 
@@ -283,7 +445,7 @@ static void MX_ADC1_Init(void)
   ADC_MultiModeTypeDef multimode;
   ADC_ChannelConfTypeDef sConfig;
 
-    /**Common config 
+    /**Common config
     */
   hadc1.Instance = ADC1;
   hadc1.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
@@ -304,7 +466,7 @@ static void MX_ADC1_Init(void)
     _Error_Handler(__FILE__, __LINE__);
   }
 
-    /**Configure the ADC multi-mode 
+    /**Configure the ADC multi-mode
     */
   multimode.Mode = ADC_MODE_INDEPENDENT;
   if (HAL_ADCEx_MultiModeConfigChannel(&hadc1, &multimode) != HAL_OK)
@@ -312,7 +474,7 @@ static void MX_ADC1_Init(void)
     _Error_Handler(__FILE__, __LINE__);
   }
 
-    /**Configure Regular Channel 
+    /**Configure Regular Channel
     */
   sConfig.Channel = ADC_CHANNEL_1;
   sConfig.Rank = ADC_REGULAR_RANK_1;
@@ -368,14 +530,14 @@ static void MX_I2C1_Init(void)
     _Error_Handler(__FILE__, __LINE__);
   }
 
-    /**Configure Analogue filter 
+    /**Configure Analogue filter
     */
   if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
   {
     _Error_Handler(__FILE__, __LINE__);
   }
 
-    /**Configure Digital filter 
+    /**Configure Digital filter
     */
   if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
   {
@@ -410,9 +572,9 @@ static void MX_SPI1_Init(void)
 
 }
 
-/** Configure pins as 
-        * Analog 
-        * Input 
+/** Configure pins as
+        * Analog
+        * Input
         * Output
         * EVENT_OUT
         * EXTI
@@ -427,17 +589,18 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, PrechargeEnded_Pin|TS_ON_Pin|BMS_Fault_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, PrechargeEnded_Pin|CS_6820_Pin|CS_SDCard_Pin|TS_ON_Pin
+                          |BMS_Fault_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, CS_ADC_PackV_Pin|CS_6820_Pin|CS_SDCard_Pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(CS_ADC_PackV_GPIO_Port, CS_ADC_PackV_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(EEPromWc_GPIO_Port, EEPromWc_Pin, GPIO_PIN_SET);
 
-  /*Configure GPIO pins : PrechargeEnded_Pin CS_ADC_PackV_Pin CS_6820_Pin CS_SDCard_Pin 
+  /*Configure GPIO pins : PrechargeEnded_Pin CS_ADC_PackV_Pin CS_6820_Pin CS_SDCard_Pin
                            TS_ON_Pin BMS_Fault_Pin */
-  GPIO_InitStruct.Pin = PrechargeEnded_Pin|CS_ADC_PackV_Pin|CS_6820_Pin|CS_SDCard_Pin 
+  GPIO_InitStruct.Pin = PrechargeEnded_Pin|CS_ADC_PackV_Pin|CS_6820_Pin|CS_SDCard_Pin
                           |TS_ON_Pin|BMS_Fault_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
@@ -507,7 +670,7 @@ void _Error_Handler(char *file, int line)
   * @retval None
   */
 void assert_failed(uint8_t* file, uint32_t line)
-{ 
+{
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
      tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
